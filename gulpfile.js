446b65746c646cce5864c2deb2627d3e85e9573c
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-12-05 23:06:40 -0300

    incluir codigo de otros arch con import

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-function tarea(done) {
-    console.log('mi primer tarea');
+const {src, dest, watch} = require("gulp");
+const sass = require("gulp-sass")(require('sass'));
+function css(done) {
+    src('src/scss/app.scss') //identificar el archivo de SASS
+        .pipe(sass())     //compilarlo
+        .pipe(dest('build/css')); //almacenar en el disco duro
+
+    done(); // Callback que avisa a gulp cuando llegamos al final
+}
+
+function dev(done) {
+    watch("src/scss/app.scss", css)
     done();
 }
-exports.primerTarea = tarea;
+
+exports.css = css;
+exports.dev = dev;
